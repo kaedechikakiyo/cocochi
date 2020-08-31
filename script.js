3334f5f1d9b3df4e119d939deca977520f319b7d
Kaede Chikakiyo <kaede@Kaedes-MacBook-Pro-5.local>
2020-08-31 21:51:06 +0900

    メイキングを修正

--- script.js
+++ script.js
@@ -177,3 +177,32 @@ $(function() {
           ScrollReveal().reveal('.what-list li');
       
   });
+
+
+  $(function() {
+    $('.single-item').slick();
+});
+  
+  $(function() {
+    $('.multiple-item').slick({
+          infinite: true,
+          dots:true,
+          slidesToShow: 6,
+          slidesToScroll: 6,
+          responsive: [{
+               breakpoint: 768,
+                    settings: {
+                         slidesToShow: 3,
+                         slidesToScroll: 3,
+               }
+          },{
+               breakpoint: 480,
+                    settings: {
+                         slidesToShow: 2,
+                         slidesToScroll: 2,
+                    }
+               }
+          ]
+     });
+});
+
